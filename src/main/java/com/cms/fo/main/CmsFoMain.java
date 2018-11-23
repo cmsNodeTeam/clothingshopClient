e57c1267285a666ca63b75920d1e1d620ee73efd
CC <CC@CC.workgroup>
2018-11-23 18:37:39 +0800

    1

--- src/main/java/com/cms/fo/main/CmsFoMain.java
+++ src/main/java/com/cms/fo/main/CmsFoMain.java
@@ -1,16 +1,8 @@
 package com.cms.fo.main;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class CmsFoMain {
 
 	public static void main(String[] args) {
-		List<String> objs = new ArrayList<>();
-		objs.add("aa");
-		objs.add("bb");
-		objs.forEach(s -> {
-			System.out.println(s);
-		});
+		System.out.println("成功");
 	}
 }
